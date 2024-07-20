7256f19f5c636ec49cccc993dd1c0676a6569f38
light2k4 <augustin.pro.prs@gmail.com>
2024-07-20 21:37:40 +0200

    chore: Update server port to 9000

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
 const app = express();
+app.listen(9000);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
